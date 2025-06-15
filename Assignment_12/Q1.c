052243afdb7e142249b23f7cd974cb10e3a460f0
Revati Ponkshe <107451174+revatip-20@users.noreply.github.com>
2025-06-15 20:32:02 +0530

    Add files via upload

---
+++ Assignment_12/Q1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    if (iRow < 0)
+        iRow = -iRow;
+    if (iCol < 0)
+        iCol = -iCol;
+
+    int i = 0;
+    for (int i = 1; i <= iRow; i++)
+    {
+        for (int j = 1; j <= iCol; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and cols: ");
+    scanf("%d %d", &iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
