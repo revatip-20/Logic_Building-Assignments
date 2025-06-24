f7c2f2acd3b1f66ed19d3ecf9b767eefcf2d82b1
Revati Ponkshe <107451174+revatip-20@users.noreply.github.com>
2025-06-24 15:09:40 +0530

    Add files via upload

---
+++ Assignment_15/Q2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    if (iRow < 0)
+        iRow = -iRow;
+    if (iCol < 0)
+        iCol = -iCol;
+
+    int i = 0, j = 0;
+
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+            if (j <= iCol - i + 1)
+                printf("* ");
+            else
+                printf("# ");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and cols: ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
---
+++ Assignment_15/Q5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    if (iRow < 0)
+        iRow = -iRow;
+    if (iCol < 0)
+        iCol = -iCol;
+
+    int i = 0, j = 0;
+
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+            
+            if (i == 1 || i == iRow)
+            {
+                printf("%d ", j);
+            }
+            
+            else if (j == 1 || j == iCol)
+            {
+                printf("%d ", j);
+            }
+          
+            else
+            {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows and cols: ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
